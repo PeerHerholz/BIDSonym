1389351d36ff1e0eaceca6fdd54c50584e2ea6af
PeerHerholz <herholz.peer@gmail.com>
2019-10-17 09:09:46 -0400

    add option to build different image types locally

--- generate_images.sh
+++ generate_images.sh
@@ -30,10 +30,10 @@ generate_docker() {
              --run-bash "git clone https://github.com/mih/mridefacer" \
              --env MRIDEFACER_DATA_DIR=/mridefacer/data \
              --run-bash "rm -r /usr/share/fsl/data/atlases && rm -r /usr/share/fsl/data/first && rm -r /usr/share/fsl/data/possum" \
-             --copy bidsonym/bidsonym.py /home/run.py \
+             --copy bidsonym/bidsonym.py /home/bidsonym.py \
              --copy bidsonym/version /home/version \
              --copy bidsonym/fs_data /home/fs_data \
-             --entrypoint "/neurodocker/startup.sh python /home/run.py"
+             --entrypoint "/neurodocker/startup.sh python /home/bidsonym.py"
 }
 
 generate_singularity() {
@@ -61,16 +61,34 @@ generate_singularity() {
             --run-bash "git clone https://github.com/mih/mridefacer" \
             --env MRIDEFACER_DATA_DIR=/mridefacer/data \
             --run-bash "rm -r /usr/share/fsl/data/atlases && rm -r /usr/share/fsl/data/first && rm -r /usr/share/fsl/data/possum" \
-            --copy bidsonym/bidsonym.py /home/run.py \
+            --copy bidsonym/bidsonym.py /home/bidsonym.py \
             --copy bidsonym/version /home/version \
             --copy bidsonym/fs_data /home/fs_data \
-            --entrypoint "/neurodocker/startup.sh python /home/run.py"
+            --entrypoint "/neurodocker/startup.sh python /home/bidsonym.py"
 }
 
 # generate files
 generate_docker > Dockerfile
 generate_singularity > Singularity
 
-# Build images using the saved files
-docker build -t bidsonym:test .
-singularity build bidsonym.simg Singularity
+# check if images should be build locally or not
+if [ '$1' = 'local' ]; then
+  if [ '$2' = 'docker' ]; then
+    echo "docker image will be build locally"
+    # build image using the saved files
+    docker build -t eegprep:test .
+  elif [ '$2' = 'singularity']; then
+    echo "singularity image will be build locally"
+    # build image using the saved files
+    singularity build eegprep.simg Singularity
+  elif [ '$2' = 'both' ]; then
+    echo "docker and singularity images will be build locally"
+    # build images using the saved files
+    docker build -t eegprep:test .
+    singularity build eegprep.simg Singularity
+  elif [ -z "$2" ]; then
+    echo "Please indicate which image should be build. You can choose from docker, singularity or both."
+  fi
+else
+  echo "Image(s) won't be build locally."
+fi
