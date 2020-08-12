a1ba27cde42cc6fb17de948cff582cd9c89ed389
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-08-12 23:22:29 +0000

    Update docs after building Travis build 79 of PeerHerholz/BIDSonym
    The docs were built from the branch 'master' against the commit
    52d75cdc16c60a7ee60a541ada3ecd98dbbe1d84.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/PeerHerholz/BIDSonym/jobs/717428345.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1.post4+g2c98d8a',
+    VERSION: '0.0.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
