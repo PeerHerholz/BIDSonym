c97eb37ca7e819ec34d319c659c3b8c44ad06dad
PeerHerholz <PeerHerholz@users.noreply.github.com>
2023-10-24 15:09:53 +0000

    Create container_build_publish.yml 1d2bfc0abbbd3250c7531251c1e51f622a7582fb

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,6 @@
-const DOCUMENTATION_OPTIONS = {
-    VERSION: '0+untagged.1.ga1e8ee6',
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0+untagged.1.g1d2bfc0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
