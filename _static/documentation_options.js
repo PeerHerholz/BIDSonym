793dbda1e0482f0fc0fb48d50fe20a759be80de7
PeerHerholz <PeerHerholz@users.noreply.github.com>
2024-02-21 16:29:42 +0000

    Update Dockerfile 2ad461f8dc378d2dff8968996dafc0a262072f21

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,4 +1,5 @@
-const DOCUMENTATION_OPTIONS = {
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0+untagged.1.g2ad461f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
