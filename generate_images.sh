f1b153df18ec2553547da89785edb3c32dfaad2f
Yaroslav Halchenko <debian@onerussian.com>
2024-06-12 09:33:46 -0400

    Nth attempt to update docker setup, newer neurodocker, some settings from neurodesk fsl recipe
    failed with FSL installation failure
    
    ERROR occurred during installation!
        This command returned an error: /opt/fsl-6.0.6.4/bin/conda env update
    -p /opt/fsl-6.0.6.4 -f /tmp/tmp_ynzfnbz/fsl-6.0.6.4_linux-64.yml

--- generate_images.sh
+++ generate_images.sh
@@ -4,20 +4,23 @@
 
  set -e
 
+# TODO: try/switch to FSL 6.0.7.4 after fresh neurodocker gets released
 generate_docker() {
-  docker run --rm kaczmarj/neurodocker:0.6.0 generate docker \
-             --base ubuntu:20.04 \
+  yes | docker run -i --rm repronim/neurodocker:0.9.5 generate docker \
+             --base-image ubuntu:20.04 \
              --pkg-manager apt \
-             --install git num-utils gcc g++ curl yarn build-essential nano git-annex npm\
+	     --env DEBIAN_FRONTEND=noninteractive \
+             --install git num-utils gcc g++ curl yarn build-essential nano git-annex npm \
              --run-bash "curl -sL https://deb.nodesource.com/setup_15.x | bash - && apt update && apt-get install -y nodejs"\
              --run-bash "npm install -g bids-validator@1.9.9" \
-             --fsl version=6.0.1 method=binaries \
+             --fsl version=6.0.6.4 method=binaries \
              --miniconda \
-                conda_install="python=3.10 numpy nipype nibabel pandas datalad" \
+                version=latest \
+                env_name='bidsonym' \
+		env_exists=false \
+		conda_install="python=3.10 numpy nipype nibabel pandas datalad" \
                 pip_install='tensorflow scikit-image pydeface==2.0.2 nobrainer==0.4.0 quickshear==1.2.0 datalad-osf pybids==0.16.4' \
-                create_env='bidsonym' \
-                activate=true \
-             --run-bash "git config --global user.email "bidsonym@example.com" && git config --global user.name "BIDSonym"" \
+             --run-bash "git config --global user.email 'bidsonym@example.com' && git config --global user.name 'BIDSonym'" \
              --run-bash "mkdir -p /opt/nobrainer/models && cd /opt/nobrainer/models && source activate bidsonym && datalad clone https://github.com/neuronets/trained-models && cd trained-models && git-annex enableremote osf-storage && datalad get -s osf-storage neuronets/brainy/0.1.0/weights/brain-extraction-unet-128iso-model.h5" \
              --run-bash "mkdir /home/mri-deface-detector && cd /home/mri-deface-detector && npm install sharp --unsafe-perm && npm install -g mri-deface-detector --unsafe-perm && cd ~" \
              --run-bash "git clone https://github.com/miykael/gif_your_nifti && cd gif_your_nifti && source activate bidsonym && python setup.py install" \
